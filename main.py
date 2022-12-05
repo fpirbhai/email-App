fc1a0c8a5765cab5569a694c607579e6e0aa0e0d
pirbhai14 <pirbhai14@hotmail.com>
2022-12-05 23:56:20 +0400

    News and email implemented

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import send_email
+
 
 
 api_key = 'af08a8752fcd4bb0b473429f62029f1f'
@@ -9,10 +11,16 @@ url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-11-05&sortBy=publishe
 request = requests.get(url,verify=False)
 content = request.json()
 
-for index, article in enumerate(content['articles']):
-    print(index)
-    print(f"Title: {article['title']}")
-    print(f"Description: {article['description']}")
-    print()
+msg = ''
 
+for index, article in enumerate(content['articles']):
+    if article['title'] is not None:
+        msg = msg + article['title'] +"\n"+ article['description'] + "\n" + "\n"
+
+msg = msg.encode('utf-8')   
+
+send_email.send_email(msg)
+
+# print(msg)
+print('Message Sent')
 
---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib, ssl
+import os
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    username = "zafrip14@gmail.com"
+    password = os.getenv('PASSWORD')
+
+    receiver = "zafrip14@gmail.com"
+
+    context = ssl.create_default_context()
+
+    print(message)
+
+    with smtplib.SMTP_SSL(host, port, context = context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+if __name__ == "__main__":
+    send_email("This is a test.")
