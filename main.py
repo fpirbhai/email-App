10a5331808a2b2083f45906cd04bc18d8254e6df
pirbhai14 <pirbhai14@hotmail.com>
2022-12-06 00:17:28 +0400

    D28: Add url links, limit results, set language n set topic

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import send_email
 
-
+topic = 'Deloitte'
 
 api_key = 'af08a8752fcd4bb0b473429f62029f1f'
 
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-11-05&sortBy=publishedAt&apiKey=af08a8752fcd4bb0b473429f62029f1f'
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2022-11-05&sortBy=publishedAt&apiKey=af08a8752fcd4bb0b473429f62029f1f&language=en"
 
 
 request = requests.get(url,verify=False)
@@ -13,11 +13,14 @@ content = request.json()
 
 msg = ''
 
-for index, article in enumerate(content['articles']):
+for index, article in enumerate(content['articles'][:20]):
+
     if article['title'] is not None:
-        msg = msg + article['title'] +"\n"+ article['description'] + "\n" + "\n"
+        msg = msg + article['title'] +"\n"+ article['description'] + "\n" + article['url']+ 2*"\n"
 
 msg = msg.encode('utf-8')   
+
+msg = f'Subject: Today {topic} News\n'.encode('utf-8') + msg
 
 send_email.send_email(msg)
 
--- send_email.py
+++ send_email.py
@@ -12,8 +12,6 @@ def send_email(message):
 
     context = ssl.create_default_context()
 
-    print(message)
-
     with smtplib.SMTP_SSL(host, port, context = context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
